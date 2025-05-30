1104a64c1800ae7d484fb8bb742733c2ace3f8ff
Drasec_Goner <drasec.04@gmail.com>
2025-05-31 03:07:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for form submissions
+
+const personRoutes = require('./routes/personRoutes');
+app.use('/person', personRoutes);
+
+// Add this route to redirect root to /person
+app.get('/', (req, res) => {
+    res.redirect('/person');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB Connected');
+        app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB:', err);
+        process.exit(1);
+    }
+);
